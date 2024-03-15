597ccf22d5b1af801f75c34de64bbf08dfd7c5f6
sreekanth221998 <sreekanthkuncha@gmail.com>
2024-03-15 15:45:14 +0100

    change count_json script

--- src/scripts/count_json_files.py
+++ src/scripts/count_json_files.py
@@ -1,9 +1,17 @@
 import os
-import glob
+from glob import glob
+from utils import obtain_chembench_repo
+from paths import output as output_path
+
 
 def count_json_files_in_directory(directory):
-    json_files = glob.glob(os.path.join(directory, '*.json'))
+    json_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.json'):
+                json_files.append(os.path.join(root, file))
     return len(json_files)
+
 
 def process_directory(directory):
     total_json_files = 0
@@ -12,12 +20,20 @@ def process_directory(directory):
         if os.path.isdir(sub_dir_path) and sub_dir != 'smarts':
             if sub_dir == 'safety':
                 total_json_files += process_safety_directory(sub_dir_path)
+            elif sub_dir == 'icho':
+                num_json_files = count_json_files_in_directory(sub_dir_path)
+                total_json_files += num_json_files
+                with open(output_path / "json_file_counts.txt", 'a') as f:
+                    f.write(f'{sub_dir} has {num_json_files} .json files\n')
             else:
                 num_json_files = count_json_files_in_directory(sub_dir_path)
                 total_json_files += num_json_files
-                print(f'{sub_dir} has {num_json_files} .json files')
-    
-    print(f'Total number of JSON files in {directory}: {total_json_files}')
+                with open(output_path / "json_file_counts.txt", 'a') as f:
+                    f.write(f'{sub_dir} has {num_json_files} .json files\n')
+
+    with open(output_path / "json_file_counts.txt", 'a') as f:
+        f.write(f'Total number of JSON files in {directory}: {total_json_files}\n')
+
 
 def process_safety_directory(safety_dir):
     total_safety_json_files = 0
@@ -26,13 +42,17 @@ def process_safety_directory(safety_dir):
         if os.path.isdir(safety_subdir_path):
             num_json_files = count_json_files_in_directory(safety_subdir_path)
             total_safety_json_files += num_json_files
-            print(f'{safety_subdir} of safety has {num_json_files} .json files')
-    
+            with open(output_path / "json_file_counts.txt", 'a') as f:
+                f.write(f'{safety_subdir} of safety has {num_json_files} .json files\n')
+
     return total_safety_json_files
 
+
 def main():
-    root_dir = '/home/ta45woj/chem-bench/data'
+    chembench_repo = obtain_chembench_repo()
+    root_dir = os.path.join(chembench_repo, "data")
     process_directory(root_dir)
+
 
 if __name__ == '__main__':
     main()
