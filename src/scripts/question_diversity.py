d9f341d3d3825baca3182ef59e547ae0beb09739
Kevin Maik Jablonka <mail@kjablonka.com>
2024-03-15 13:53:15 +0100

    feat: add embedding plotting script
    co-authored-by: Adrian Mirza <adrianmirza81@gmail.com>

---
+++ src/scripts/question_diversity.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from sklearn.decomposition import PCA
+import seaborn as sns
+from paths import data, scripts, figures
+from utils import ONE_COL_GOLDEN_RATIO_HEIGHT_INCH, ONE_COL_WIDTH_INCH
+
+plt.style.use(scripts / "lamalab.mplstyle")
+
+
+def load_embeddings_and_labels():
+    # read npy file
+    embeddings = np.load(data / "gpt_embeddings/embeddings.npy")
+    labels = np.load(data / "gpt_embeddings/labels.npy")
+    return embeddings, labels
+
+
+def plot_pca_map():
+    embeddings, labels = load_embeddings_and_labels()
+    labels = [label.split("(")[0] for label in labels]
+    # PCA
+    embeddings_pca = PCA(n_components=2).fit_transform(embeddings)
+    pca_1, pca_2 = embeddings_pca[:, 0], embeddings_pca[:, 1]
+
+    # shuffle
+    np.random.seed(0)
+    shuffled_indices = np.random.permutation(len(pca_1))
+    pca_1 = pca_1[shuffled_indices]
+    pca_2 = pca_2[shuffled_indices]
+    labels = np.array(labels)[shuffled_indices]
+    f = plt.figure(figsize=(ONE_COL_WIDTH_INCH, ONE_COL_GOLDEN_RATIO_HEIGHT_INCH))
+    # place legend outside of the plot
+
+    plt.axis("off")
+    sns.scatterplot(x=pca_1, y=pca_2, hue=labels, s=100, alpha=0.7)
+    f.savefig(figures / "question_diversity.pdf", bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    plot_pca_map()
