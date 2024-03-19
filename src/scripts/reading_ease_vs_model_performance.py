eade1a98a4337ec2e99cf9340074147965cf30f4
Kevin Maik Jablonka <mail@kjablonka.com>
2024-03-19 22:11:56 +0100

    update reading ease figure

--- src/scripts/reading_ease_vs_model_performance.py
+++ src/scripts/reading_ease_vs_model_performance.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import numpy as np
 from paths import scripts, output, figures, data
 import pickle
 
-from utils import ONE_COL_GOLDEN_RATIO_HEIGHT_INCH, ONE_COL_WIDTH_INCH
+from utils import (
+    ONE_COL_GOLDEN_RATIO_HEIGHT_INCH,
+    ONE_COL_WIDTH_INCH,
+    TWO_COL_GOLDEN_RATIO_HEIGHT_INCH,
+    TWO_COL_WIDTH_INCH,
+)
 import seaborn as sns
 
 from plotutils import range_frame, model_color_map
@@ -33,9 +38,58 @@ if __name__ == "__main__":
 
     all_aligned = pd.concat(all_aligned)
 
-    fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-    sns.scatterplot(
-        data=all_aligned, x="reading_ease", y="all_correct", hue="model", ax=ax
+    fig, ax = plt.subplots(
+        1, 1, figsize=(TWO_COL_WIDTH_INCH, TWO_COL_GOLDEN_RATIO_HEIGHT_INCH)
     )
+
+    relevant_models = [
+        "gpt4",
+        "claude2",
+        "claude3",
+        "llama70b",
+        "gemini_pro",
+        "galactica_120b",
+    ]
+
+    all_aligned = all_aligned[all_aligned["model"].isin(relevant_models)]
+
+    sns.violinplot(
+        data=all_aligned,
+        x="all_correct_",
+        y="flesch_kincaid_reading_ease",
+        hue="model",
+        ax=ax,
+        palette=model_color_map,
+    )
+
+    # change labels for the colors
+    # to be more readable
+    handles, labels = ax.get_legend_handles_labels()
+    name_replace = {
+        "gpt4": "GPT-4",
+        "claude2": "Claude 2",
+        "claude3": "Claude 3",
+        "llama70b": "Llama 70B",
+        "gemini_pro": "Gemini Pro",
+        "galactica_120b": "Galactica 120B",
+    }
+    labels = [name_replace[label] for label in labels]
+    ax.legend(handles, labels, ncol=3, bbox_to_anchor=(0.8, 1.2))
+
+    ax.set_xlabel("completly correct")
+    ax.set_ylabel("Flesch-Kincaid Reading Ease")
+
+    range_frame(
+        ax,
+        np.array([-0.5, 1.5]),
+        np.array(
+            [
+                all_aligned["flesch_kincaid_reading_ease"].min(),
+                all_aligned["flesch_kincaid_reading_ease"].max(),
+            ]
+        ),
+    )
+
+    fig.tight_layout()
 
     fig.savefig(figures / "reading_ease_vs_model_performance.pdf", bbox_inches="tight")
