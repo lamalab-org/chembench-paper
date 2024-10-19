3f168d9bd6dfa5638778d91fcc1e45431f5ef12e
Kevin Maik Jablonka <mail@kjablonka.com>
2024-10-19 19:46:59 +0200

    cleanup

--- src/data/chem-bench-main/src/chembench/__init__.py
+++
@@ -1,4 +0,0 @@
-from loguru import logger
-
-logger.disable("chembench")
-__version__ = "0.2.0-dev"
--- src/data/chem-bench-main/src/chembench/types.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-from typing import Union
-
-from typing_extensions import TypeAlias
-
-PathType: TypeAlias = Union[str, Path]
