4f1ebbdb50d8b91ba66e79853e01620191adf03b
MrtinoRG <martinriosgarcia@gmail.com>
2024-10-24 00:15:44 +0200

    fix: correct performance table to be naturally sorted

--- src/scripts/make_performance_tables.py
+++ src/scripts/make_performance_tables.py
@@ -2,6 +2,11 @@ import pickle
 from typing import Optional
 from loguru import logger
 
+import natsort
+from natsort import (
+    natsort_key,
+    natsorted
+)
 
 from paths import output
 
@@ -158,7 +163,7 @@ def make_table(
     # filter out rows where ("Model", "") starts with a number
     df = df[~df[("Model", "")].str.match(r"^\d")]
     # Sort the DataFrame alphabetically by the column "Models"
-    df = df.sort_values(by=("Model", ""))
+    df = df.reindex(natsort.natsorted(df.index, key=lambda x: df.loc[x, 'Model'], alg=natsort.IC))
 
     # Function to bold the best value (assuming higher is better)
     def bold_best(series):
